f1a8205ce54f6b31c4e309ac5faf435b20507f64
Almun <almontaser2012@hotmail.com>
2023-12-13 16:03:39 +0100

    Output as a table

--- src/main/java/org/dkfz/DNAColorBalanceCalculator.java
+++ src/main/java/org/dkfz/DNAColorBalanceCalculator.java
@@ -7,18 +7,27 @@ public class DNAColorBalanceCalculator {
     public void processSequences(List<String> sequences){
         int len = sequences.get(0).length();
 
+        // Kopfzeile der Tabelle
+        System.out.println("------------------------");
+        System.out.println("| Zyklus | Rot  | Grün |");
+        System.out.println("---------|------|-------");
+        // logic for each cycle of the laser scanner (1..n)---> len
+        // iter over each cycle
         for (int i = 0; i < len; i++){
             int redVisible = 0;
             int greenVisible = 0;
 
+            // iter over each sequence
             for (String seq: sequences){
                 char base = seq.charAt(i);
                 if (isVisibleUnderRedLaserPhoto(base)) redVisible++;
                 if (isVisibleUnderGreenLaserPhoto(base)) greenVisible++;
             }
+            System.out.printf("| %-6d | %-5d| %-5d| %n", i + 1, redVisible, greenVisible);
 
-            System.out.println("- Red: " + redVisible +" |"+" greed: " + greenVisible);
         }
+        System.out.println("------------------------");
+
     }
 
     public boolean isVisibleUnderRedLaserPhoto(char base){
